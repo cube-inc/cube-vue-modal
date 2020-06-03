f7942975bfee531ef7a2c59c6de7c028204ae219
Fred Gomez <fred.titao@gmail.com>
2020-06-03 02:44:04 +0200

    Updates CubeModal.spec

--- tests/unit/CubeModal.spec.js
+++ tests/unit/CubeModal.spec.js
@@ -1,11 +1,14 @@
 import { mount } from '@vue/test-utils'
-import Modal from '@/components/CubeModal'
+import Modal from '@/components/CubeModal.vue'
 
 describe(Modal.name, () => {
   it('sets default props properly', () => {
     const wrapper = mount(Modal)
-    expect(wrapper.name()).toBe('CubeModal')
+    expect(Modal.name).toBe('CubeModal')
     expect(wrapper.props()).toHaveProperty('value', false)
-    expect(wrapper.props()).toHaveProperty('transition', 'animate')
+    expect(wrapper.props()).toHaveProperty('closeButton', true)
+    expect(wrapper.props()).toHaveProperty('transitionName', 'animate')
+    expect(wrapper.props()).toHaveProperty('target')
+    expect(wrapper.props().target).toBeInstanceOf(HTMLBodyElement)
   })
 })
